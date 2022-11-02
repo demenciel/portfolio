308834ecc30469211d34e058ce62e212fca45c29
Alex Couture <79432799+demenciel@users.noreply.github.com>
2022-11-02 09:59:19 -0400

    Add files via upload
    Upload js

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-const navbar = document.querySelector(".navbar");
-const navbarOffsetTop = navbar.offsetTop;
-const sections = document.querySelectorAll("section");
-const navbarLinks = document.querySelectorAll(".navbar-link");
-const progress = document.querySelector(".progress-bars-wrapper");
-const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
+var navbar = document.querySelector(".navbar");
+var sections = document.querySelectorAll("section");
+var navbarLinks = document.querySelectorAll(".navbar-link");
+var progress = document.querySelector(".progress-bars-wrapper");
+var progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
-window.addEventListener("scroll", () => {
-  mainFn();
-});
+document.addEventListener('DOMContentLoaded', function () {
 
-const mainFn = () => {
-  if (window.pageYOffset >= navbarOffsetTop) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
-  }
-
-  sections.forEach((section, i) => {
-    if (window.pageYOffset >= section.offsetTop - 10) {
-      navbarLinks.forEach((navbarLink) => {
-        navbarLink.classList.remove("change");
-      });
-      navbarLinks[i].classList.add("change");
-    }
+  window.addEventListener("scroll", () => {
+    mainFn();
   });
 
-  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-    document.querySelectorAll(".progress-percent").forEach((el, i) => {
-      el.style.width = `${progressBarPercents[i]}%`;
-      el.previousElementSibling.firstElementChild.textContent =
-        progressBarPercents[i];
+  function mainFn() {
+    if (window.pageYOffset >= navbar.offsetTop) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
+
+    sections.forEach((section, i) => {
+      if (window.pageYOffset >= section.offsetTop - 10) {
+        navbarLinks.forEach((navbarLink) => {
+          navbarLink.classList.remove("change");
+        });
+        navbarLinks[i].classList.add("change");
+      }
     });
-  }
-};
 
-mainFn();
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+      document.querySelectorAll(".progress-percent").forEach((el, i) => {
+        el.style.width = `${progressBarPercents[i]}%`;
+        el.previousElementSibling.firstElementChild.textContent =
+          progressBarPercents[i] + "%";
+      });
+    }
+  };
 
-window.addEventListener("resize", () => {
-  window.location.reload();
+  mainFn();
+
+  window.addEventListener("resize", () => {
+    window.location.reload();
+  });
 });
-
