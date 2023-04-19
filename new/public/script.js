464dfc21157537d4909be7d1b9d871fd6137e7cb
Alexandre Couture <acouture@c1r7p6.42quebec.com>
2023-04-19 13:23:56 -0400

    home finish

--- new/public/script.js
+++ new/public/script.js
@@ -1,4 +1,50 @@
 const container = document.getElementById("body");
+const navLinks = document.getElementById("navbar");
+const menuButton = document.querySelector(".menu-collapse");
+let menuIsOn = true;
+var i = 0;
+var y = 0;
+var txt = ["Code Connoisseur", "Engineer of Innovation", "Solutions Architect", "Tech Tactician"];
+var speed = 60;
+var typeWriterId = document.getElementById("developper-type");
+
+
+menuButton.addEventListener("click", (e) => {
+  if (menuIsOn) {
+    navLinks.style.animation = "slideMenuOff 1s forwards";
+    menuIsOn = false;
+  }
+  else if (menuIsOn == false) {
+    navLinks.style.animation = "slideMenuOn 1s forwards";
+    menuIsOn = true;
+  }
+});
+
+function typeWriter() {
+  if (i < txt[y].length) {
+    typeWriterId.innerHTML += txt[y].charAt(i);
+    // typeWriterId.innerHTML.style.colors = '#4F4557';
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+  else {
+    setTimeout(resetTypeWriter, 1000);
+  }
+}
+
+function resetTypeWriter() {
+  if (i > 0) {
+    typeWriterId.innerHTML = typeWriterId.innerHTML.slice(0, -1);
+    i--;
+    setTimeout(resetTypeWriter, speed);
+  } else {
+    y++;
+    if (y == 3)
+      y = 0;
+    typeWriter();
+  }
+}
+typeWriter();
 
 container.addEventListener("mousemove", (e) => {
   createBubble(e.pageX, e.pageY);
